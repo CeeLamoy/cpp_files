b2fe3f2440acd85bb1c4b47006a28c44e30007cc
CeeLamoy <32014415+CeeLamoy@users.noreply.github.com>
2019-07-29 14:03:08 +0600

    Add files via upload
    takes a number from the user and prints out the number, until it gets to 1.

---
+++ whileLoop.cpp
@@ -0,0 +1,18 @@
+# include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Starting number: ";
+    cin>>n;
+
+    while (n>0)
+    {
+      cout<<n<<",";
+      --n;
+    }
+
+
+    return 0;
+}
