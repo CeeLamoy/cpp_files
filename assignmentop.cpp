da337669069a168d1065f35b463143ef81569822
CeeLamoy <32014415+CeeLamoy@users.noreply.github.com>
2019-07-29 13:40:09 +0600

    Add files via upload
    demonstrates the use of assignment operators

---
+++ assignmentop.cpp
@@ -0,0 +1,19 @@
+// assignment operator
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    a = 10;
+    b = 4;
+    a = b;
+    b = 7;
+
+    cout<<"a: ";
+    cout<<a<<endl;
+    cout<<"b: ";
+    cout<<b;
+
+    return 0;
+}
