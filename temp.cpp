71d08be454c8312ee1e4bc55082792780b44a64a
CeeLamoy <32014415+CeeLamoy@users.noreply.github.com>
2019-07-29 13:57:24 +0600

    Add files via upload
    asks user for the temperature in degrees and converts it to Farenheit

---
+++ temp.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float temp, x;
+    cout<<"Temperature: ";
+    cin>>temp;
+
+    x= ((1.82 * temp)+ 32);
+    cout<<x;
+    return 0;
+}
