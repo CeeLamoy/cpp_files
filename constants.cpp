9aa1aa1c644cc046c775aa6c45ba01d967f890de
CeeLamoy <32014415+CeeLamoy@users.noreply.github.com>
2019-07-29 13:46:05 +0600

    Add files via upload
    finds the area of a circle by use of a constant PI defined in the program.

---
+++ constants.cpp
@@ -0,0 +1,15 @@
+//circumference of a circle
+# include <iostream>
+using namespace std;
+
+#define PI 3.141959
+
+int main()
+{
+    double r = 5.0;
+    double circumference;
+
+    circumference = 2 * PI * r;
+    cout<<circumference;
+    return 0;
+}
